15356d81532b644bcbde1c502c5c3ee068490efa
Kangbo Lu <kangbolu@pc56.cs.ucdavis.edu>
2018-03-14 17:07:55 -0700

    update comment for ProblemA.R

--- ProblemA.R
+++ ProblemA.R
@@ -7,9 +7,9 @@
 # Overall quiz average.
 
 # Preparing data for statistical analysis
-# ================================
-# creating ECS 132 quiz data frame
-# ================================
+# ==================================================================
+# creating data frame includes ECS 132, ECS 145, ECS 158 quiz data
+# ================================================================
 quiz_132_f13 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/132/F13.txt", header = FALSE)
 quiz_132_f13 <- cbind(rep("2013.3", nrow(quiz_132_f13)), quiz_132_f13)
 colnames(quiz_132_f13) <- c("year offered", "major", "quiz average")
@@ -31,9 +31,7 @@ quiz_132_data <- cbind(rep("ECS 132", nrow(quiz_132_data)), quiz_132_data)
 colnames(quiz_132_data) <- c("course name", "year offered", "major", "quiz average")
 # quiz_132_data
 
-# ================================
 # creating ECS 145 quiz data frame
-# ================================
 quiz_145_f14 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/145/F14.txt", header = FALSE)
 quiz_145_f14 <- cbind(rep("2014.3", nrow(quiz_145_f14)), quiz_145_f14)
 colnames(quiz_145_f14) <- c("year offered", "major", "quiz average")
@@ -47,9 +45,7 @@ quiz_145_data <- cbind(rep("ECS 145", nrow(quiz_145_data)), quiz_145_data)
 colnames(quiz_145_data) <- c("course name", "year offered", "major", "quiz average")
 # quiz_145_data
 
-# ================================
 # creating ECS 158 quiz data frame
-# ================================
 quiz_158_f13 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/158/F13.txt", header = FALSE)
 quiz_158_f13 <- cbind(rep("2013.3", nrow(quiz_158_f13)), quiz_158_f13)
 colnames(quiz_158_f13) <- c("year offered", "major", "quiz average")
@@ -76,9 +72,10 @@ student_quiz <- rbind(quiz_132_data, quiz_145_data, quiz_158_data)
 major_name_modifer <- function(name) ifelse(name=="LCSI", "CS", "CSE")
 student_quiz["major"] <- lapply(student_quiz["major"], major_name_modifer)
 
-#=================================================================
+#=======================================================================
+# 95% Confidence Interval for each course's population mean quiz average
+#=======================================================================
 # 95% Confidence Interval for ECS 132 population mean quiz average
-#=================================================================
 ECS132_sample_mean <- mean(quiz_132_data[["quiz average"]])
 ECS132_sample_variance <- mean(quiz_132_data[["quiz average"]]^2) - ECS132_sample_mean^2
 ECS132_sample_std <- sqrt(ECS132_sample_variance)
@@ -86,9 +83,7 @@ radius <- 1.96 * ECS132_sample_std / sqrt(nrow(quiz_132_data))
 cat("Approx. 95% CI for population mean quiz average for ECS132 =",
     ECS132_sample_mean - radius,"to", ECS132_sample_mean + radius,"\n")
 
-#=================================================================
 # 95% Confidence Interval for ECS 145 population mean quiz average
-#=================================================================
 ECS145_sample_mean <- mean(quiz_145_data[["quiz average"]])
 ECS145_sample_variance <- mean(quiz_145_data[["quiz average"]]^2) - ECS145_sample_mean^2
 ECS145_sample_std <- sqrt(ECS145_sample_variance)
@@ -96,9 +91,7 @@ radius <- 1.96 * ECS145_sample_std / sqrt(nrow(quiz_145_data))
 cat("Approx. 95% CI for population mean quiz average for ECS145 =",
     ECS132_sample_mean - radius,"to", ECS145_sample_mean + radius,"\n")
 
-#=================================================================
 # 95% Confidence Interval for ECS 158 population mean quiz average
-#=================================================================
 ECS158_sample_mean <- mean(quiz_158_data[["quiz average"]])
 ECS158_sample_variance <- mean(quiz_158_data[["quiz average"]]^2) - ECS158_sample_mean^2
 ECS158_sample_std <- sqrt(ECS158_sample_variance)
