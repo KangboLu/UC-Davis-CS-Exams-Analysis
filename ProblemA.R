b03c6011d43350489723786bf0bf3b35df093982
Kangbo Lu <redstonegranola@gmail.com>
2018-03-20 09:46:29 -0700

    stay up to date with master branch

--- ProblemA.R
+++ ProblemA.R
@@ -11,19 +11,19 @@
 # creating ECS 132 quiz data frame
 # ================================
 quiz_132_f13 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/132/F13.txt", header = FALSE)
-quiz_132_f13 <- cbind(rep("2013.3", nrow(quiz_132_f13)), quiz_132_f13)
+quiz_132_f13 <- cbind(rep(2013.3, nrow(quiz_132_f13)), quiz_132_f13)
 colnames(quiz_132_f13) <- c("year offered", "major", "quiz average")
 # quiz_132_f13
 quiz_132_w15 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/132/W15.txt", header = FALSE)
-quiz_132_w15 <- cbind(rep("2015.1", nrow(quiz_132_w15)), quiz_132_w15)
+quiz_132_w15 <- cbind(rep(2015.1, nrow(quiz_132_w15)), quiz_132_w15)
 colnames(quiz_132_w15) <- c("year offered", "major", "quiz average")
 # quiz_132_w15
 quiz_132_w16 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/132/W16.txt", header = FALSE)
-quiz_132_w16 <- cbind(rep("2016.1", nrow(quiz_132_w16)), quiz_132_w16)
+quiz_132_w16 <- cbind(rep(2016.1, nrow(quiz_132_w16)), quiz_132_w16)
 colnames(quiz_132_w16) <- c("year offered", "major", "quiz average")
 # quiz_132_w16
 quiz_132_w17 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/132/W17.txt", header = FALSE)
-quiz_132_w17 <- cbind(rep("2017.1", nrow(quiz_132_w17)), quiz_132_w17)
+quiz_132_w17 <- cbind(rep(2017.1, nrow(quiz_132_w17)), quiz_132_w17)
 colnames(quiz_132_w17) <- c("year offered", "major", "quiz average")
 # quiz_132_w17
 quiz_132_data <- rbind(quiz_132_f13, quiz_132_w15, quiz_132_w16, quiz_132_w17)
@@ -35,11 +35,11 @@ colnames(quiz_132_data) <- c("course name", "year offered", "major", "quiz avera
 # creating ECS 145 quiz data frame
 # ================================
 quiz_145_f14 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/145/F14.txt", header = FALSE)
-quiz_145_f14 <- cbind(rep("2014.3", nrow(quiz_145_f14)), quiz_145_f14)
+quiz_145_f14 <- cbind(rep(2014.3 , nrow(quiz_145_f14)), quiz_145_f14)
 colnames(quiz_145_f14) <- c("year offered", "major", "quiz average")
 # quiz_145_f14
 quiz_145_w17 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/145/W17.txt", header = FALSE)
-quiz_145_w17 <- cbind(rep("2017.1", nrow(quiz_145_w17)), quiz_145_w17)
+quiz_145_w17 <- cbind(rep(2017.1, nrow(quiz_145_w17)), quiz_145_w17)
 colnames(quiz_145_w17) <- c("year offered", "major", "quiz average")
 # quiz_145_w17
 quiz_145_data <- rbind(quiz_145_f14, quiz_145_w17)
@@ -51,19 +51,19 @@ colnames(quiz_145_data) <- c("course name", "year offered", "major", "quiz avera
 # creating ECS 158 quiz data frame
 # ================================
 quiz_158_f13 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/158/F13.txt", header = FALSE)
-quiz_158_f13 <- cbind(rep("2013.3", nrow(quiz_158_f13)), quiz_158_f13)
+quiz_158_f13 <- cbind(rep(2013.3, nrow(quiz_158_f13)), quiz_158_f13)
 colnames(quiz_158_f13) <- c("year offered", "major", "quiz average")
 # quiz_158_f13
 quiz_158_f17 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/158/F17.txt", header = FALSE)
-quiz_158_f17 <- cbind(rep("2017.3", nrow(quiz_158_f17)), quiz_158_f17)
+quiz_158_f17 <- cbind(rep(2017.3, nrow(quiz_158_f17)), quiz_158_f17)
 colnames(quiz_158_f17) <- c("year offered", "major", "quiz average")
 # quiz_158_f17
 quiz_158_s16 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/158/S16.txt", header = FALSE)
-quiz_158_s16 <- cbind(rep("2016.2", nrow(quiz_158_s16)), quiz_158_s16)
+quiz_158_s16 <- cbind(rep(2016.2, nrow(quiz_158_s16)), quiz_158_s16)
 colnames(quiz_158_s16) <- c("year offered", "major", "quiz average")
 # quiz_158_s16
 quiz_158_w15 <- read.table("http://heather.cs.ucdavis.edu/~matloff/132/Hwk/ProblemAData/158/W15.txt", header = FALSE)
-quiz_158_w15 <- cbind(rep("2017.3", nrow(quiz_158_w15)), quiz_158_w15)
+quiz_158_w15 <- cbind(rep(2017.3, nrow(quiz_158_w15)), quiz_158_w15)
 colnames(quiz_158_w15) <- c("year offered", "major", "quiz average")
 # quiz_158_w15
 quiz_158_data <- rbind(quiz_158_f13, quiz_158_f17, quiz_158_s16, quiz_158_w15)
@@ -71,7 +71,7 @@ quiz_158_data <- cbind(rep("ECS 158", nrow(quiz_158_data)), quiz_158_data)
 colnames(quiz_158_data) <- c("course name", "year offered", "major", "quiz average")
 # quiz_158_data
 
-# Combine 132, 145, and 159 data and modify course name column data
+# Combine 132, 145, and 158 data and modify course name column data
 student_quiz <- rbind(quiz_132_data, quiz_145_data, quiz_158_data)
 #major_name_modifer <- function(name) ifelse(name=="LCSI", "CS", "CSE")
 #student_quiz["major"] <- lapply(student_quiz["major"], major_name_modifer)
@@ -112,12 +112,10 @@ cat("Approx. 95% CI for population mean quiz average for ECS158 =",
 #================================================================================
 # 95% Confidence Interval for the difference in ECS132 and ECS145 Population Mean
 #================================================================================
-# p_hat1 − p_hat2 ± R
-p_hat1 <- ECS132_sample_mean
-p_hat2 <- ECS145_sample_mean
+# ECS132_sample_mean − ECS145_sample_mean ± R
 R <- 1.96 * sqrt( (ECS132_sample_variance/nrow(quiz_145_data)) + (ECS145_sample_variance/nrow(quiz_145_data)) )
 cat("\nApprox. 95% CI for the difference in ECS132 and ECS145 Population Mean =", 
-    p_hat1 - p_hat2 - R,"to", p_hat1 - p_hat2 + R,"\n")
+    ECS132_sample_mean - ECS145_sample_mean - R,"to", ECS132_sample_mean - ECS145_sample_mean + R,"\n")
 
 #==============================================================================================================
 # 95% Confidence Interval for the difference in population mean quiz averages in ECS 145 between the two majors
@@ -134,10 +132,13 @@ R <- 1.96 * sqrt( (varCSEin145/length(CSin145)) + (varCSin145/length(CSin145)) )
 cat("\nApprox. 95% CI for the difference in population mean quiz averages in ECS 145 between the two majors =",
     p_hat1 - p_hat2 - R,"to", p_hat1 - p_hat2 + R,"\n")
 
-majorcodes <- as.integer(student_quiz[["major"]])
+# creating dummy variables for 'major' and 'course name'
+majorcodes <- as.integer(student_quiz$major)
 is_cs_major <- as.integer(majorcodes==2)
-coursecodes <- as.integer(student_quiz[["course name"]])
-is_ECS132 <- as.integer(coursecodes==1)
+coursecodes <- as.integer(student_quiz$`course name`)
 is_ECS145 <- as.integer(coursecodes==2)
+is_ECS158 <- as.integer(coursecodes==3)
 
-summary(lm(student_quiz$`quiz average` ~ student_quiz$`year offered` + is_cs_major + is_ECS132 + is_ECS145))
+summary(lm(student_quiz$`quiz average` ~ student_quiz$`year offered` + is_cs_major + is_ECS145 + is_ECS158))
+
+summary(lm(is_cs_major ~ student_quiz$`year offered`))
